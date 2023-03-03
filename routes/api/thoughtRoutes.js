88bb4cfdd46c489ff066948b6355a445bcfb3eac
Jeremiah Chesley <msgrmiah@gmail.com>
2023-03-03 14:05:54 -0700

    starting on routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController');
+
+//thoughs
+
+//by thought id
+
+//by reaction
+
+//react id
+
+module.exports = router;
